c26d6a1bb012a50767e4a313594199d3d56e26fc
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-08-31 09:45:42 +0530

    Delete pg14.py

--- pg14.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = {
-    'customer_age': [25, 30, 35, 25, 40, 30, 35, 30, 45, 25, 35, 40]
-}
-sales_data = pd.DataFrame(data)
-age_freq = sales_data['customer_age'].value_counts().reset_index()
-age_freq.columns = ['Age', 'Frequency']
-age_freq = age_freq.sort_values(by='Age')
-print(age_freq)
