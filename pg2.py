6a1a17df6ad1be8d62a8f6a69cb054aa34c8c60b
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-08-31 08:36:32 +0530

    Update pg2.py

--- pg2.py
+++ pg2.py
@@ -5,3 +5,5 @@ sales_data = np.array([[100, 200, 150],
 average_price = np.mean(sales_data)
 
 print("Average Price of All Products Sold in the Past Month:", average_price)
+OUTPUT:
+Average Price of All Products Sold in the Past Month: 103.33333333333333
